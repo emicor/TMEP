d0470a04421089ab10610b355fc186e70e671940
emicor <wecos87@gmail.com>
2015-01-11 02:41:52 +0100

    Create es.php
    Spanish translation :D

---
+++ app/scripts/language/es.php
@@ -0,0 +1,115 @@
+<?php
+
+ /*************************************************************************
+ ***  Sistema de TME/TH2E - TMEP                                        ***
+ ***  (c) Michal Ševčík 2007-2013 - multi@tricker.cz                    ***
+ ***  Traducción al español                                             ***
+ *************************************************************************/
+
+  // nadpis
+  if($vlhkomer == 1)
+  {
+    $lang['titulekstranky'] = "TH2E - Termómetro e Higrómetro Ethernet";
+    $lang['popisstranky'] = "Termómetro e Higrómetro Ethernet TH2E - página con estadísticas y gráficos.";
+    $lang['hlavninadpis'] = "TH2E - Termómetro e Higrómetro Ethernet";
+  }
+  else
+  {
+    $lang['titulekstranky'] = "TME - Termómetro Ethernet";
+    $lang['popisstranky'] = "Termómetro Ethernet TME - página con estadísticas y gráficos.";
+    $lang['hlavninadpis'] = "TME - Termómetro Ethernet";
+  }
+
+  $lang['statistika'] = "Estadísticas";
+  $lang['umisteni'] = "Lugar:";
+  $lang['pocetmereni'] = "Mediciones:";
+  $lang['nejvyssiteplota'] = "Temperatura Máx.";
+  $lang['nejnizsiteplota'] = " Temperatura Mín.";
+  $lang['drivetoutodobou'] = "Otros días a esta hora";
+  $lang['cas'] = "Hora";
+  $lang['den'] = "Día";
+  $lang['dnes'] = "Hoy";
+  $lang['teplota'] = "Temperatura";
+  $lang['aktualniteplota'] = "Temperatura actual";
+  $lang['paticka'] = "Fabricante TME: <a href='http://www.papouch.com' target='_blank'>www.papouch.com</a>, autor de esta aplicación: <a href='http://multi.tricker.cz' target='_blank'>multi.tricker.cz</a>, gráficos generados usando <a href='http://www.highcharts.com/'>Highcharts</a>.";
+  $lang['teplota4hodiny'] = "Temperatura de las últimas 4 horas";
+  $lang['teplota24hodin'] = "Temperatura de las últimas 24 horas";
+  $lang['teplota3dny'] = "Últimos 3 días - detalle curva";
+  $lang['teploty31dni'] = "Últimos 31 días";
+  $lang['prumernamesicnigraf'] = "Temperaturas último mes";
+  $lang['min'] = "Mínima";
+  $lang['max'] = "Máxima";
+  $lang['avg'] = "Media";
+  $lang['cas'] = "Hora";
+  $lang['grafy'] = "Gráficas";
+  $lang['jazyk'] = "Idioma";
+  $lang['jednotka'] = "Escala Temp.";
+
+  // TMEP v3
+  $lang['mesic01'] = "Enero";
+  $lang['mesic02'] = "Febrero";
+  $lang['mesic03'] = "Marzo";
+  $lang['mesic04'] = "Abril";
+  $lang['mesic05'] = "Mayo";
+  $lang['mesic06'] = "Junio";
+  $lang['mesic07'] = "Julio";
+  $lang['mesic08'] = "Augosto";
+  $lang['mesic09'] = "Septiembre";
+  $lang['mesic10'] = "Octubre";
+  $lang['mesic11'] = "Noviembre";
+  $lang['mesic12'] = "Diciembre";
+  $lang['mesic'] = "Mes";
+  $lang['dennistatistiky'] = "Estadísticas diarias";
+  $lang['mesicnistatistiky'] = "Estadísticas mensuales";
+  $lang['rocnistatistiky'] = "Estadísticas anuales";
+  $lang['nejteplejsimesice'] = "Mes más cálido";
+  $lang['nejchladnejsimesice'] = "Mes más frío";
+  $lang['prumernateplota'] = "Temperatura media";
+  $lang['nejvicemereni'] = "Más medidas.";
+  $lang['nejmenemereni'] = "Menos medidas.";
+  $lang['mereni'] = "Mediciones";
+  $lang['nenidostatecnypocethodnot'] = "No hay suficientes medidas guardadas";
+  $lang['min2'] = "MÍN";
+  $lang['prumer'] = "AVG";
+  $lang['max2'] = "MÁX";
+  $lang['doba'] = "Período";
+  $lang['nejnizsiteploty'] = "Temperatura mínima.";
+  $lang['nejvyssiteploty'] = "Temperatura máxima";
+  $lang['teplotyzaposlednidny'] = "Temperaturas de los últimos días";
+
+  // TMEP v4
+  $lang['merenood'] = "Datos desde";
+  $lang['aktualnivlhkost'] = "Humedad actual:";
+  $lang['grafyteploty'] = "Gráficas de temperatura";
+  $lang['grafyvlhkosti'] = "Gráficas de humedad";
+  $lang['historie'] = "Histórico";
+  $lang['vlhkost'] = "Humedad";
+  $lang['vlhkost4hodiny'] = "Humedad últimas 4 horas";
+  $lang['vlhkost24hodin'] = "Humedad últimas 24 horas";
+  $lang['vlhkost3dny'] = "Humedad últimos 3 días - detalle curva";
+  $lang['vlhkost31dni'] = "Humedad últimos 31 días";
+  $lang['mesicnivlhkostgraf'] = "Humedad mensual";
+  $lang['nejnizsivlhkost'] = "Humedad mínima";
+  $lang['nejvyssivlhkost'] = "Humedad máxima";
+  $lang['prumernavlhkost'] = "Humedad media";
+  $lang['vlhkostzaposlednidny'] = "Humedad en los últimos días";
+  $lang['nejvlhcimesice'] = "Meses más húmedos";
+  $lang['nejsussimesice'] = "Meses más secos";
+  $lang['zobrazitden'] = "Ver día";
+  $lang['zobrazit'] = "Mostrar";
+  $lang['od'] = "Desde";
+  $lang['do'] = "Hasta";
+  $lang['nenalezenyzaznam'] = "Este día no se ha encontrado en la base de datos.";
+  $lang['namerenehodnotyvprubehulet'] = "Mediciones de los últimos años";
+  $lang['hodnotynamerenevjednotlivychdobach'] = "Valores medios por períodos";
+
+  // TMEP v4.1
+  $lang['rosnybod'] = "Punto de rocío";
+  $lang['4hodiny'] = "Últimas 4 horas";
+  $lang['24hodin'] = "Últimas 24 horas";
+  $lang['3dny'] = "Últimos 3 días";
+  $lang['hodnota'] = "Valor";
+  $lang['aktualne'] = "Actualmente";
+
+  // TMEP 4.2
+  $lang['kompletnistranky'] = "Vista completa";
